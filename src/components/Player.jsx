a7da0709c66fdcbe29d8fa5e024e907ad1eb5327
Igor Matlingiewicz <igor.matlingiewicz@gmail.com>
2024-12-23 17:13:01 +0100

    Added winning logic and game over screen, and better players names handling

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing((editing) => !editing)
+        setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
@@ -16,7 +20,7 @@ export default function Player({ initialName, symbol, isActive }) {
     // let btnCaption = "Edit";
 
     if (isEditing) {
-        editablePlayerName = <input type="" required value={playerName} onChange={handleChange}/>
+        editablePlayerName = <input type="" required value={playerName} onChange={handleChange} />
         // btnCaption = 'Save';
     }
 
